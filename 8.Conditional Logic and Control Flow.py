9d0128e17b2594214f43448934c5fd7be530f58b
Aleksandra <119521509+aleksandraenkrol@users.noreply.github.com>
2023-10-17 11:16:03 +0200

    looking for the factor of a number

--- 8.Conditional Logic and Control Flow.py
+++ 8.Conditional Logic and Control Flow.py
@@ -111,3 +111,32 @@ if number == 3:
     print("You win!")
 else:
     print("You lose.")
+
+
+
+#8.4 Challenge: Find the Factors of a Number
+"""
+A factor of a positive integer n is any positive integer less than or equal to n that divides n with no remainder.
+For example, 3 is a factor of 12 because 12 divided by 3 is 4 with no remainder. However, 5 is not a factor of 12
+because 5 goes into 12 twice with a remainder of 2.
+
+Write a program called factors.py that asks the user to input a positive integer and then prints out the factors of that number.
+Here's a sample run of the program with output:
+
+Enter a positive integer: 12
+1 is a factor of 12
+2 is a factor of 12
+3 is a factor of 12
+4 is a factor of 12
+6 is a factor of 12
+12 is a factor of 12
+
+Hint: Recall from chapter 5 that you can use the % operator to get the remainder of dividing one number by another.
+"""
+
+num = int(input("Enter positive integer: "))
+
+for factor in range(1, num+1):
+    if num % factor == 0:
+        print(f"{factor} is a factor of 12")
+
